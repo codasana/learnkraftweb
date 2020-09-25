8f524447ff1910d6e3ad10f37e9ec4008d07365c
Manish Sahajwani <manish@learnkraft.com>
2020-09-25 14:19:35 +0530

    changes

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://relaxed-shaw-a638ec.netlify.app',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
+  title: 'Manish Sahajwani Personal Website',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
@@ -25,8 +25,8 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
+    name: 'Manish Sahajwani',
+    photo: '/manish.jpeg',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
       email: '',
